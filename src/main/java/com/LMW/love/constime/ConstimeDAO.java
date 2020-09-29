47721f195cf7d61ea5535d4e8979837e49d0e4e8
kth <ant940202@gamil.com>
2020-09-29 11:08:50 +0900

    092911

--- src/main/java/com/LMW/love/constime/ConstimeDAO.java
+++ src/main/java/com/LMW/love/constime/ConstimeDAO.java
@@ -1,5 +1,8 @@
 package com.LMW.love.constime;
 
+import java.util.HashMap;
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -9,4 +12,11 @@ public class ConstimeDAO {
 	
 	@Autowired
 	private SqlSession sqlSession;
+	
+	public List timelist() {
+		return sqlSession.selectList("mapper.Constime.timelist");
+	}
+	public void timestate(HashMap map) {
+		sqlSession.update("mapper.Constime.timestate",map);
+	}
 }
--- src/main/java/com/LMW/love/constime/ConstimeService.java
+++ src/main/java/com/LMW/love/constime/ConstimeService.java
@@ -1,5 +1,8 @@
 package com.LMW.love.constime;
 
+import java.util.HashMap;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +11,14 @@ public class ConstimeService {
 
 	@Autowired
 	private ConstimeDAO constimeDAO;
+	
+	public List timelist() {
+		return constimeDAO.timelist();
+	}
+	public void timestate(int state , String time) {
+		HashMap map = new HashMap();
+		map.put("state",state);
+		map.put("time",time);
+		constimeDAO.timestate(map);
+	}
 }
